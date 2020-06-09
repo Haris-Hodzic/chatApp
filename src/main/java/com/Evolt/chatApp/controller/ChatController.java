cfe9567b6144a49623679dfdfbf8978276b9b3d4
Haris Hodzic <haris.hodzic.03@gmail.com>
2020-06-09 03:58:26 +0200

    fixed errors

--- src/main/java/com/Evolt/chatApp/controller/ChatController.java
+++ src/main/java/com/Evolt/chatApp/controller/ChatController.java
@@ -4,12 +4,15 @@ import com.Evolt.chatApp.model.Message;
 import com.Evolt.chatApp.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 
-@Controller
+@RestController
 @RequestMapping(value = "/api")
 public class ChatController {
 
--- src/main/java/com/Evolt/chatApp/controller/ConnectionController.java
+++ src/main/java/com/Evolt/chatApp/controller/ConnectionController.java
@@ -7,12 +7,14 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class ConnectionController {
 
     @Autowired
